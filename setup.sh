719aa31fe7cbc53b5b2b8657ccafe1e3e093becf
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-09-12 15:49:44 +0530

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"sandur43@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
